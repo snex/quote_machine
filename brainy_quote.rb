c9c2a2bb35f476cfd2c276eab3a76bcfe31476ce
Dave Havlicek <xens@comcast.net>
2016-04-23 10:56:38 -0500

    re-order these lines to make it simple to uncomment

--- brainy_quote.rb
+++ brainy_quote.rb
@@ -10,9 +10,9 @@ module BrainyQuote
     req.url = "https://www.brainyquote.com/search_results.html?q=#{CGI.escape(name)}"
     res = HTTPI.get(req)
     doc = Nokogiri::HTML(res.body)
-    #doc.xpath("//span[@class='bqQuoteLink']").map(&:inner_text)
-    # only grab 1 quote for testing to avoid API rate limit
     [doc.xpath("//span[@class='bqQuoteLink']")[0].inner_text]
+    # only grab 1 quote for testing to avoid API rate limit
+    #doc.xpath("//span[@class='bqQuoteLink']").map(&:inner_text)
   end
 
 end
